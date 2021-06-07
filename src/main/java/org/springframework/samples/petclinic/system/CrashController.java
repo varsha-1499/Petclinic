3f84299d3a2abb3b558cfd4ba30296b469804f79
Vishwas Dave (vishwas) <davevishwas98@gmail.com>
2021-06-07 12:33:11 +0000

    Update CrashController.java

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -30,7 +30,7 @@ class CrashController {
 
 	@GetMapping("/oups")
 	public String triggerException() {
-		throw new RuntimeException(
+		throw new RuntimeException(; 
 				"Expected: controller used to showcase what " + "happens when an exception is thrown");
 	}
 
