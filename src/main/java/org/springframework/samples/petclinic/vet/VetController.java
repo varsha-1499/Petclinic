3e0011d7c18a7b247fbc73a21fad490d16186c3d
Akash Patel <akpatel950@gmail.com>
2021-06-07 12:32:13 +0000

    Update VetController.java

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -40,17 +40,21 @@ class VetController {
 	public String showVetList(Map<String, Object> model) {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects so it is simpler for Object-Xml mapping
-		Vets vets = new Vets()  
+		Vets vets = new Vets();
 		vets.getVetList().addAll(this.vets.findAll());
 		model.put("vets", vets);
 		return "vets/vetList";
+	}
+
+	{
+		
 	}
 
 	@GetMapping({ "/vets" })
 	public @ResponseBody Vets showResourcesVetList() {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects so it is simpler for JSon/Object mapping
-		Vets vets = new Vets()  
+		Vets vets = new Vets();
 		vets.getVetList().addAll(this.vets.findAll());
 		return vets;
 	}
